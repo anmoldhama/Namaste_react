3ada21fed964175ae5e52c270fe514b1eef7baef
Anmol Dhama <anmol.dhama@omlogistics.co.in>
2024-07-18 16:32:28 +0530

    Lecure 03 commit

--- app.js
+++ app.js
@@ -1,16 +1,44 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 // ****************nested childs***************
-// const heading = React.createElement("div", {}, 
-//                 React.createElement("div", {}, 
+// const heading = React.createElement("div", {}f}, 
 //                 React.createElement("h1", {}, "hello world from react!")
 //                 ));
 
 
 //******************Siblings*********************
-const heading = React.createElement("div",{}, 
-                React.createElement("div",{}, [React.createElement("h1",{}, "this is the first sibling"),
-                    React.createElement("h1",{}, "this is the second sibling")
-                ]));
+// const heading = React.createElement("div",{}, 
+//                 React.createElement("div",{}, [React.createElement("h1",{}, "this is the first sibling"),
+//                     React.createElement("h1",{}, "this is the second sibling")
+//                 ]));
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+//**************** JSX syntax**********************
+
+// const jsxHeading = (<h1>Namaste javascript with jsx</h1>);
+
+//*****************Functional components ************************
+
+const Title = ()=>{
+    return (
+        <h1>title</h1>
+    )
+}
+const JsxHeading = ()=>{
+    return (
+   <div>
+    <Title />
+    <h1>Namaste javascript with functianal components</h1>
+    </div>
+    )
+}
+
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(
+
+<JsxHeading />
+
+);
